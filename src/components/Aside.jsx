7d0462671ed3b686a40aa13d5e53b6947cc1b87a
emrecoban <fbdarktole@gmail.com>
2023-03-29 21:28:07 +0300

    Added producthunt button

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -11,6 +11,7 @@ export default function Aside(){
                         <h4>💭 Post a funny caption</h4>
                         <h4>🔺 Upvote your favorites</h4>
                     </div>
+                    <a href="https://www.producthunt.com/posts/caption-meow?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-caption&#0045;meow" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=386540&theme=light" alt="Caption&#0032;Meow - social&#0032;network | Product Hunt" style={{width:"180px", height:"54px"}} width="180px" height="54" /></a>
                 </div>
                 <div className="footer">
                     &copy; 2023 Caption Meow · <a href="https://github.com/emrecoban/caption-meow" target="_blank">GitHub</a>
